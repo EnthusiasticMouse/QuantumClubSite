3ba738809686ff1c58ccd6f05ba12f8558a0a3de
EnthusiasticMouse <90762677+EnthusiasticMouse@users.noreply.github.com>
2025-04-03 21:29:52 +0100

    implemented join now button

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-var titles = document.getElementsByClassName("title");
-var heading = 90;
-var topScalar = Math.sin((heading - 90) * (Math.PI / 180));
-var leftScalar = Math.cos((heading - 90) * (Math.PI / 180));
-for(let i = 0; i < titles.length; i++){
-    titles[i].style.top = ((i * 1.5)*topScalar+40) + "px";
-    titles[i].style.left = ((i * 1.5)*leftScalar + 20) + "px";
-    if(i < titles.length -1){
-        titles[i].style.color = "hotpink";
-
-        titles[i].style.filter = "hue-rotate(" + (25 * i) +"deg)"
+var flipped = false;
+function flipContainer(){
+    var box = document.getElementById("boxToFlip");
+    if(flipped){
+        box.style.transform = "rotateY(0)";
+    }else{
+        box.style.transform = "rotateY(180deg)";
     }
+    flipped = !flipped;
 }
-
-// function updateHeading(){
-//     var element = document.getElementById("angleChanger");
-//     heading = element.value;
-//     var topScalar = Math.sin((heading - 90) * (Math.PI / 180));
-//     var leftScalar = Math.cos((heading - 90) * (Math.PI / 180));
-//     for(let i = 0; i < titles.length; i++){
-//         titles[i].style.top = ((i * 1.5)*topScalar+40) + "px";
-//         titles[i].style.left = ((i * 1.5)*leftScalar) + "px";
-//         if(i < titles.length -1){
-//             titles[i].style.color = "hotpink";
-    
-//             titles[i].style.filter = "hue-rotate(" + (25 * i) +"deg)"
-//         }
-//     }
-    
-// }
